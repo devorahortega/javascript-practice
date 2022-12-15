1035886a3925dda92868c1da81077ca1ff8eb613
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-15 13:05:05 -0600

    Adds yesterday's journal

---
+++ practiceplan.js
@@ -0,0 +1,23 @@
+// 2022-12-14
+
+// Focusing on frontend development for the next few weeks: 
+
+// -  Javascript - write deliberate practice in javascript 
+// -  Get good at functional methods - map, reduce and filter - parsing over info
+// -  Testing - learn how to do testing - jest or exorcism - do javascript track
+
+// ASSIGNMENTS:
+
+// -  Create a blank folder - set up a javascrop practice program - package json and npm - devote different files to different types of learning
+// -  Build up a portfolio of projects
+// -  Build muscle memory for javascript
+// -  Get basic programs running - make simple arrays and use functional methods
+
+// SITES FOR PRACTICE:
+
+// -  Exorcism.io - use this for basics - javascript track
+// -  Project oiler - math problems you have to figure out with code
+// -  Heroku - 7 a month - place to store the projects I make - portfolio builder
+// -   Raspberry Pie - get good at navigating it - navigate a system with your keyboard
+
+// HOMEWORK ASSIGNMENT: Get 2 done - get exorcism javascript on computer 
