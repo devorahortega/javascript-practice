0dbfa3af5dabc82138c6f67ae654b7a2cedbb40c
Devorah Ortega <devorahb.ortega@gmail.com>
2023-01-03 11:55:11 -0600

    Adds practice journal for today:

---
+++ practicejan3.js
@@ -0,0 +1,16 @@
+// 2023-01-03
+
+
+// WHAT I DID TODAY:
+
+// I am currently logging the work I am doing on something that is not pushable to GitHub. 
+
+// - Work on creating a wireframe for the database and website 
+// - Create a list of questions I need to ask before creating the website
+
+
+// HOMEWORK ASSIGNMENT:
+
+// - Have meeting to discuss the specifics of the database
+// - Message bootcamp classmates to discuss the API's used to create graphs
+// - Continue work on Exersism
