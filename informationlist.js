4b1292dfa363a2fe5ed2f093ec39921ce93948a4
Devorah Ortega <devorahb.ortega@gmail.com>
2023-01-03 12:03:32 -0600

    Adds resources to page

---
+++ informationlist.js
@@ -0,0 +1,12 @@
+// List of reserouces and information to create a statistics website:
+
+// RESOURCES:
+
+// RAILS MIGRATIONS: https://github.com/actualize-chi-2021-08/resources/blob/main/guide_rails_migrations.md
+
+// RAILS COLUMNS MIGRATIONS: https://stackoverflow.com/questions/11889048/is-there-documentation-for-the-rails-column-types
+
+// RAILS API CRUD: https://github.com/actualize-chi-2021-08/resources/blob/main/guide_rails_api_crud.md
+
+// RAILS JSON SERIALIZER: ttps://github.com/actualize-chi-2021-08/resources/blob/main/guide_rails_json_serializer.md
+
